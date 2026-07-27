0f546cc9fab0e3e1c32ecc448afd4f3b49aa9f75
Lucas Dias <glucasdiasofc@gmail.com>
2026-07-27 13:10:08 -0300

    94

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    LÓGICA: Controle de Interface, Prévias de Fotos e Validações
    ================================================================= */
 
-const VERSAO_ATUAL = "v0.0.93 - versão de teste";
+const VERSAO_ATUAL = "v0.0.94 - versão de teste";
 
 // Executa assim que a página termina de carregar no navegador
 document.addEventListener("DOMContentLoaded", () => {
@@ -2304,10 +2304,11 @@ async function salvarAlteracoesItemAdmin() {
         btn.disabled = false;
         btn.textContent = "Salvar";
     }
-
+}
 
 // Função auxiliar para upload (Reutilizando o padrão do seu app)
 async function subirImagemParaNuvem(arquivo) {
+
     try {
         let urlUpload = "";
 
