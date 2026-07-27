39a54b6b7a47138816e27eebe148cbcc3dd5c418
Lucas Dias <glucasdiasofc@gmail.com>
2026-07-27 14:36:45 -0300

    100

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    LÓGICA: Controle de Interface, Prévias de Fotos e Validações
    ================================================================= */
 
-const VERSAO_ATUAL = "v0.0.99 - versão de teste";
+const VERSAO_ATUAL = "v0.1.0 - versão de teste";
 
 // Executa assim que a página termina de carregar no navegador
 document.addEventListener("DOMContentLoaded", () => {
