f44ea366180f11e190a493280d2e031471f78d0a
Lucas Dias <glucasdiasofc@gmail.com>
2026-08-09 17:19:34 -0300

    0,107

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    LÓGICA: Controle de Interface, Prévias de Fotos e Validações
    ================================================================= */
 
-const VERSAO_ATUAL = "v0.106.0 - versão alpha";
+const VERSAO_ATUAL = "v0.107.0 - versão alpha";
 
 // Esta variável guardará o avatar padrão dos usuários e será atualizada pelo banco
 window.AVATAR_USUARIO_PADRAO = "https://res.cloudinary.com/dkozbm1ik/image/upload/v1720640000/avatar-padrao.png";
@@ -10457,9 +10457,18 @@ async function abrirComentariosPublicacao(
                                         <span>@${username}</span>
                                     </div>
 
-                                    <div class="feed-x-reels-comentario-texto">
-                                        ${textoComentario}
-                                    </div>
+<div
+    class="feed-x-reels-comentario-texto"
+    style="
+        display: block !important;
+        width: 100% !important;
+        margin: 2px 0 0 0 !important;
+        padding: 0 !important;
+        text-align: left !important;
+        align-self: flex-start !important;
+        justify-self: flex-start !important;
+    "
+>${textoComentario}</div>
 
                                 </div>
 
