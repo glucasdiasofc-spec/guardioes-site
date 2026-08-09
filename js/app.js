44cd6770fe07bcda1fa9db1f385f39ac68d4a2f0
Lucas Dias <glucasdiasofc@gmail.com>
2026-08-09 16:57:32 -0300

    0,103

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    LÓGICA: Controle de Interface, Prévias de Fotos e Validações
    ================================================================= */
 
-const VERSAO_ATUAL = "v0.102.0 - versão alpha";
+const VERSAO_ATUAL = "v0.103.0 - versão alpha";
 
 // Esta variável guardará o avatar padrão dos usuários e será atualizada pelo banco
 window.AVATAR_USUARIO_PADRAO = "https://res.cloudinary.com/dkozbm1ik/image/upload/v1720640000/avatar-padrao.png";
@@ -9298,20 +9298,14 @@ function criarCardPublicacao(
                 </div>
 
                 ${
-                    autorCargo
-                        ? `
-                            <div
-                                style="
-                                    color:#71767b;
-                                    font-size:13px;
-                                    margin-bottom:4px;
-                                "
-                            >
-                                ${autorCargo}
-                            </div>
-                        `
-                        : ""
-                }
+    autorCargo
+        ? `
+            <div class="feed-x-cargo">
+                ${autorCargo}
+            </div>
+        `
+        : ""
+}
 
 ${
     texto
