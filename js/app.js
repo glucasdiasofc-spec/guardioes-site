edccc574e021ab0fa6249df6886adbd7a0c86342
Lucas Dias <glucasdiasofc@gmail.com>
2026-08-10 13:09:01 -0300

    0,125

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    LÓGICA: Controle de Interface, Prévias de Fotos e Validações
    ================================================================= */
 
-const VERSAO_ATUAL = "v0.124.0 - versão alpha";
+const VERSAO_ATUAL = "v0.125.0 - versão alpha";
 
 // Esta variável guardará o avatar padrão dos usuários e será atualizada pelo banco
 window.AVATAR_USUARIO_PADRAO = "https://res.cloudinary.com/dkozbm1ik/image/upload/v1720640000/avatar-padrao.png";
@@ -1270,6 +1270,15 @@ function abrirSalaChat(usernameAlvo, nomeAlvo, cargoAlvo, fotoAlvo) {
 
                 telaChat.style.top =
                     `${deslocamentoSuperior}px`;
+
+                if (cabecalhoChat) {
+                    cabecalhoChat.style.transform =
+                        `translate3d(0, -${deslocamentoSuperior}px, 0)`;
+
+                    cabecalhoChat.style.transition =
+                        "none";
+                }
+
 
                 /*
                  * Mantém a caixa de mensagem
