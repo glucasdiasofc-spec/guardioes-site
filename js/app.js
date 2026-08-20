4c95fc624224fc4696befc562bce890b62b1e9cc
Lucas Dias <glucasdiasofc@gmail.com>
2026-08-20 13:55:23 -0300

    0,353

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    LÓGICA: Controle de Interface, Prévias de Fotos e Validações
    ================================================================= */
 
-const VERSAO_ATUAL = "v0.352.0 - versão alpha";
+const VERSAO_ATUAL = "v0.353.0 - versão alpha";
 
 // Esta variável guardará o avatar padrão dos usuários e será atualizada pelo banco
 window.AVATAR_USUARIO_PADRAO = "https://res.cloudinary.com/dkozbm1ik/image/upload/v1720640000/avatar-padrao.png";
@@ -8542,6 +8542,15 @@ async function renderizarPainelSecretarioFrequencia(
             const membrosDoGrupo = membros.filter(
                 membro => estados[membro.username] === estadoGrupo
             );
+            const totalGrupo = estadoGrupo === "A"
+                ? membros.filter(membro => {
+                    return estados[membro.username] === "A" ||
+                        estados[membro.username] === "J";
+                }).length
+                : membrosDoGrupo.length;
+            tituloGrupoElemento.textContent =
+                `${tituloGrupo} (${totalGrupo})`;
+
 
             if (!membrosDoGrupo.length) {
                 const vazio = document.createElement("small");
