e311f28b1f781199a2568e1817e88e7985bb46c7
Lucas Dias <glucasdiasofc@gmail.com>
2026-08-31 13:27:59 -0300

    v0.562.0

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    LÓGICA: Controle de Interface, Prévias de Fotos e Validações
    ================================================================= */
 
-const VERSAO_ATUAL = "v0.561.0 - versão alpha";
+const VERSAO_ATUAL = "v0.562.0 - versão alpha";
 
 // Esta variável guardará o avatar padrão dos usuários e será atualizada pelo banco
 window.AVATAR_USUARIO_PADRAO = "https://res.cloudinary.com/dkozbm1ik/image/upload/v1720640000/avatar-padrao.png";
@@ -16405,7 +16405,7 @@ async function renderizarPainelTesoureiroUnidade(
         formulario
     );
     telaNovoLancamento.appendChild(cartaoNovoLancamento);
-    secao.insertBefore(telaNovoLancamento, tituloLivro);
+    secao.appendChild(telaNovoLancamento);
 
 
     const voltarMensalidades = document.createElement("button");
